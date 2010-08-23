3332734dd7d473f09cec2fe1a821a26509aba643
dmik <dmik@93fcbb24-f35c-df11-b929-00e081727c95>
2010-08-23 23:52:33 +0000

    hotspot: Renamed os_os2.cpp to os_os2_init.cpp.

--- trunk/openjdk/hotspot/src/os/os2/vm/os_os2_init.cpp
+++ trunk/openjdk/hotspot/src/os/os2/vm/os_os2_init.cpp
@@ -2,6 +2,8 @@
  * Copyright 1997-2010 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
+ * Copyright 2010 netlabs.org. OS/2 Parts.
+ *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
  * published by the Free Software Foundation.
@@ -22,10 +24,8 @@
  *
  */
 
-// As the OS/2 platform currently reuses the Windows sources, here we only
-// redirect the OS/2 DLL initialization/termination calls to DllMain()
-
-#ifdef __WIN32OS2__
+// This code redirects the OS/2 DLL initialization/termination calls to
+// the Windows DllMain()
 
 #include <os2.h> // Odin32 OS/2 api wrappers
 #include <odinlx.h>
@@ -48,57 +48,56 @@ static HMODULE dllHandle = 0;
 // calling this function.
 unsigned long SYSTEM _DLL_InitTerm(unsigned long hModule, unsigned long ulFlag)
 {
-  size_t i;
-  APIRET rc;
+    size_t i;
+    APIRET rc;
 
-  // If ulFlag is zero then the DLL is being loaded so initialization should
-  // be performed.  If ulFlag is 1 then the DLL is being freed so termination
-  // should be performed. A non-zero value must be returned to indicate success.
+    // If ulFlag is zero then the DLL is being loaded so initialization should
+    // be performed.  If ulFlag is 1 then the DLL is being freed so termination
+    // should be performed. A non-zero value must be returned to indicate success.
 
-  switch (ulFlag) {
-  case 0 :
+    switch (ulFlag) {
+    case 0 :
 #ifdef __EMX__
-    // initialize the C library
-    if (_CRT_init())
-      break;
-    // initialize C++ statics
-    __ctordtorInit();
+        // initialize the C library
+        if (_CRT_init())
+          break;
+        // initialize C++ statics
+        __ctordtorInit();
 #else
 #  error "Add code to initialize C/C++ library of this compiler!"
 #endif
 
-    // check that the runtime Odin version matches the compile time version
-    // (this will issue a message box and abort the process on mismatch)
-    CheckVersionFromHMOD(PE2LX_VERSION, hModule);
+        // check that the runtime Odin version matches the compile time version
+        // (this will issue a message box and abort the process on mismatch)
+        CheckVersionFromHMOD(PE2LX_VERSION, hModule);
 
-    // enable __try/__except support
-    EnableSEH();
+        // enable __try/__except support
+        EnableSEH();
 
-    dllHandle = RegisterLxDll(hModule, DllMain, NULL);
-    if (dllHandle == 0)
-      break;
+        dllHandle = RegisterLxDll(hModule, DllMain, NULL);
+        if (dllHandle == 0)
+          break;
 
-    return 1;
+        return 1;
 
-  case 1 :
-    if (dllHandle) {
-      UnregisterLxDll(dllHandle);
+    case 1 :
+        if (dllHandle) {
+          UnregisterLxDll(dllHandle);
+        }
+#ifdef __EMX__
+        // destroy C++ statics
+        __ctordtorTerm();
+        _CRT_term();
+#else
+#  error "Add code to initialize C/C++ library of this compiler!"
+#endif
+        return 0;
+
+    default:
+        break;
     }
-#ifdef __EMX__
-    // destroy C++ statics
-    __ctordtorTerm();
-    _CRT_term();
-#else
-#  error "Add code to initialize C/C++ library of this compiler!"
-#endif
+
+    // failure
     return 0;
-
-  default:
-    break;
-  }
-
-  // failure
-  return 0;
 }
 
-#endif // __WIN32OS2__
