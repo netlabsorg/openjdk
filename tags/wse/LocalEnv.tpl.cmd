cabeb0ccf0a8a61c8426404f00bd936c57a197b0
dmik <dmik@93fcbb24-f35c-df11-b929-00e081727c95>
2011-05-12 20:53:52 +0000

    release: Tagged WSE build.

---
+++ tags/wse/LocalEnv.tpl.cmd
@@ -0,0 +1,130 @@
+/*
+ * Local environment settings for OpenJDK (Template)
+ *
+ * This file is used to specify local paths to external tools and libraries
+ * and to perform other site-specific project environment setup.
+ *
+ * NOTES:
+ *
+ *   This file is a template! Copy it to a file named LocalEnv.cmd in
+ *   the same directory and modify the copy to fit your local environment.
+ *
+ *   All paths in this file are specified using back slashes unless specified
+ *   otherwise.
+ */
+
+/**
+ * Path to JDK used for bootstrapping. Must be set.
+ *
+ * Note that when using a previous OpenJDK build here, you must make sure
+ * that all it needs (e.g. a corresponding Odin runtime) is on LIBPATH.
+ */
+G.PATH_TOOL_BOOT_JDK    = 'D:\Dev\openjdk6'
+
+/**
+ * Path to GCC 4 environment script (+ possible arguments). If you use
+ * the GCC 4 distribution recommended in README, the path will look like:
+ *
+ *   X:\path_to_gcc4\gcc4xx.cmd
+ *
+ * Otherwise you will have to play with GCC's gccenv.cmd on your own.
+ *
+ * Leave this empty if you have GCC 4 already available in your
+ * environment.
+ */
+G.PATH_TOOL_GCC4_ENV    = 'D:\Dev\gcc452\gcc452.cmd'
+
+/**
+ * Path to Apache Ant. Leave empty if you have it already available
+ * in your environment.
+ */
+G.PATH_TOOL_ANT         = 'D:\Dev\java\apache-ant-1.8.1'
+
+/**
+ * Path to Odin32 SDK. Must be set.
+ */
+G.PATH_LIB_ODIN32       = 'D:\Coding\odin32'
+
+/**
+ * Path to the previous Java SDK where components not built from the
+ * current source tree will be imported from. Normally, it is not used and
+ * should be just the same as G.PATH_TOOL_BOOT_JDK.
+ */
+G.PATH_JDK_IMPORT       = G.PATH_TOOL_BOOT_JDK
+
+/**
+ * Log file to save all console output of the build process (only works when
+ * using the SE script). Leave it empty to disable logging.
+ */
+G.LOG_FILE              = 'se.log' /* ScriptDir'se.log' */
+
+/*
+ * Advanced definitions
+ * ----------------------------------------------------------------------------
+ *
+ * Used mostly for developing and debugging.
+ */
+
+/**
+ * Path to the debug version of kLIBC DLL (used with -l flag). Optional.
+ */
+G.PATH_TOOL_KLIBC_DEBUG     = ''
+
+/**
+ * Path to the log check version of kLIBC DLL (used with -L flag). Optional.
+ */
+G.PATH_TOOL_KLIBC_LOGCHK    = ''
+
+/*
+ * Additional environment settings
+ * ----------------------------------------------------------------------------
+ *
+ * Controls various aspects of the build process.
+ */
+
+/**
+ * Directory where to find JAXP/JAXWS source bundles. If this is not set (or if
+ * the specified directory doesn't contain the bundles), ALLOW_DOWNLOADS below
+ * must be set to 'true'.
+ */
+/*
+call EnvSet 'ALT_DROPS_DIR', 'D:/Coding/java_src_drops'
+*/
+
+/**
+ * Instructs the build process to download JAXP/JAXWS source bundles if they
+ * are missing when building JAXP/JAXWS.
+ */
+call EnvSet 'ALLOW_DOWNLOADS', 'true'
+
+/**
+ * Here you may put any additional environment variable definitions needed for
+ * your local environment or for the OpenJDK make files using the form shown
+ * below. These variables are passed to the environment without any
+ * modifications.
+ */
+/*
+call EnvSet 'ALT_SOME_VAR', 'some value'
+*/
+
+/*
+ * Production definitions
+ * ----------------------------------------------------------------------------
+ *
+ * Not intended for playing around with.
+ */
+
+/**
+ * JDK build number in format 'bNN'.
+ */
+/*
+call EnvSet 'BUILD_NUMBER', 'b00'
+*/
+
+/**
+ * Milestone name. Will appear in the version string after the verison number
+ * and before the build number.
+ */
+/*
+call EnvSet 'MILESTONE', 'internal'
+*/
---
+++ tags/wse/openjdk/jdk/src/windows/javavm/export/jni_md.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 1996, 1998, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+#ifndef _JAVASOFT_JNI_MD_H_
+#define _JAVASOFT_JNI_MD_H_
+
+#define JNIEXPORT __declspec(dllexport)
+#define JNIIMPORT __declspec(dllimport)
+#define JNICALL __stdcall
+
+typedef long jint;
+#ifdef __EMX__
+typedef __int64_t jlong;
+#else
+typedef __int64 jlong;
+#endif
+typedef signed char jbyte;
+
+#ifdef __cplusplus
+
+inline void jsafe_cast_POD_type_only(...) {}
+
+/* template for safe type casting: the generic version lets the compiler
+ * decide (as if no template was used); specific instantiations deal with
+ * special cases which are guaranteed to be safe. Note that it cannot be used
+ * with non-POD types as otherwise it would implicitly call the copy ctor which
+ * may have various unwanted side effects */
+template<typename TR, typename TS>
+inline TR jsafe_cast(TS ts) { jsafe_cast_POD_type_only(ts); return ts; }
+
+#ifdef __EMX__
+/* sizeof(jchar) = sizeof(wchar_t) in GCC but the types are not relative
+ * (as opposed to MSVC) so an explicit cast is required */
+typedef unsigned short jchar;
+template<>
+inline jchar *jsafe_cast<jchar *, wchar_t *>(wchar_t *ts) { return reinterpret_cast<jchar*>(ts); }
+template<>
+inline const jchar *jsafe_cast<const jchar *, wchar_t *>(wchar_t *ts) { return reinterpret_cast<const jchar*>(ts); }
+template<>
+inline const jchar *jsafe_cast<const jchar *, const wchar_t *>(const wchar_t *ts) { return reinterpret_cast<const jchar*>(ts); }
+template<>
+inline wchar_t *jsafe_cast<wchar_t *, jchar *>(jchar *ts) { return reinterpret_cast<wchar_t*>(ts); }
+template<>
+inline const wchar_t *jsafe_cast<const wchar_t *, jchar *>(jchar *ts) { return reinterpret_cast<const wchar_t*>(ts); }
+template<>
+inline const wchar_t *jsafe_cast<const wchar_t *, const jchar *>(const jchar *ts) { return reinterpret_cast<const wchar_t*>(ts); }
+#endif
+
+#endif /* __cplusplus */
+
+#endif /* !_JAVASOFT_JNI_MD_H_ */
