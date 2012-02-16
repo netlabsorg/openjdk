9826598c57366154b6c148272faf44698679512d
Dmitriy Kuminov <coding@dmik.org>
2012-02-16 13:21:52 +0000

    Port SymGetSymFromAddr API usage to OS/2.

--- openjdk/hotspot/src/os/windows/vm/decoder_windows.cpp
+++ openjdk/hotspot/src/os/windows/vm/decoder_windows.cpp
@@ -28,7 +28,7 @@
 
 HMODULE                   Decoder::_dbghelp_handle = NULL;
 bool                      Decoder::_can_decode_in_vm = false;
-pfn_SymGetSymFromAddr64   Decoder::_pfnSymGetSymFromAddr64 = NULL;
+pfn_SymGetSymFromAddrXX   Decoder::_pfnSymGetSymFromAddrXX = NULL;
 pfn_UndecorateSymbolName  Decoder::_pfnUndecorateSymbolName = NULL;
 
 void Decoder::initialize() {
@@ -45,11 +45,15 @@ void Decoder::initialize() {
 
     pfn_SymSetOptions _pfnSymSetOptions = (pfn_SymSetOptions)::GetProcAddress(handle, "SymSetOptions");
     pfn_SymInitialize _pfnSymInitialize = (pfn_SymInitialize)::GetProcAddress(handle, "SymInitialize");
-    _pfnSymGetSymFromAddr64 = (pfn_SymGetSymFromAddr64)::GetProcAddress(handle, "SymGetSymFromAddr64");
+#ifdef __WIN32OS2__
+    _pfnSymGetSymFromAddrXX = (pfn_SymGetSymFromAddrXX)::GetProcAddress(handle, "SymGetSymFromAddr");
+#else
+    _pfnSymGetSymFromAddrXX = (pfn_SymGetSymFromAddrXX)::GetProcAddress(handle, "SymGetSymFromAddr64");
+#endif
     _pfnUndecorateSymbolName = (pfn_UndecorateSymbolName)GetProcAddress(handle, "UnDecorateSymbolName");
 
-    if (_pfnSymSetOptions == NULL || _pfnSymInitialize == NULL || _pfnSymGetSymFromAddr64 == NULL) {
-      _pfnSymGetSymFromAddr64 = NULL;
+    if (_pfnSymSetOptions == NULL || _pfnSymInitialize == NULL || _pfnSymGetSymFromAddrXX == NULL) {
+      _pfnSymGetSymFromAddrXX = NULL;
       _pfnUndecorateSymbolName = NULL;
       ::FreeLibrary(handle);
       _dbghelp_handle = NULL;
@@ -59,7 +63,7 @@ void Decoder::initialize() {
 
     _pfnSymSetOptions(SYMOPT_UNDNAME | SYMOPT_DEFERRED_LOADS);
     if (!_pfnSymInitialize(GetCurrentProcess(), NULL, TRUE)) {
-      _pfnSymGetSymFromAddr64 = NULL;
+      _pfnSymGetSymFromAddrXX = NULL;
       _pfnUndecorateSymbolName = NULL;
       ::FreeLibrary(handle);
       _dbghelp_handle = NULL;
@@ -79,7 +83,7 @@ void Decoder::initialize() {
 
 void Decoder::uninitialize() {
   assert(_initialized, "Decoder not yet initialized");
-  _pfnSymGetSymFromAddr64 = NULL;
+  _pfnSymGetSymFromAddrXX = NULL;
   _pfnUndecorateSymbolName = NULL;
   if (_dbghelp_handle != NULL) {
     ::FreeLibrary(_dbghelp_handle);
@@ -95,14 +99,14 @@ bool Decoder::can_decode_C_frame_in_vm() {
 
 Decoder::decoder_status Decoder::decode(address addr, char *buf, int buflen, int *offset) {
   assert(_initialized, "Decoder not yet initialized");
-  if (_pfnSymGetSymFromAddr64 != NULL) {
-    PIMAGEHLP_SYMBOL64 pSymbol;
-    char symbolInfo[MAX_PATH + sizeof(IMAGEHLP_SYMBOL64)];
-    pSymbol = (PIMAGEHLP_SYMBOL64)symbolInfo;
+  if (_pfnSymGetSymFromAddrXX != NULL) {
+    PIMAGEHLP_SYMBOLXX pSymbol;
+    char symbolInfo[MAX_PATH + sizeof(IMAGEHLP_SYMBOLXX)];
+    pSymbol = (PIMAGEHLP_SYMBOLXX)symbolInfo;
     pSymbol->MaxNameLength = MAX_PATH;
-    pSymbol->SizeOfStruct = sizeof(IMAGEHLP_SYMBOL64);
-    DWORD64 displacement;
-    if (_pfnSymGetSymFromAddr64(::GetCurrentProcess(), (DWORD64)addr, &displacement, pSymbol)) {
+    pSymbol->SizeOfStruct = sizeof(IMAGEHLP_SYMBOLXX);
+    DWORDXX displacement;
+    if (_pfnSymGetSymFromAddrXX(::GetCurrentProcess(), (DWORDXX)addr, &displacement, pSymbol)) {
       if (buf != NULL) {
         if (!demangle(pSymbol->Name, buf, buflen)) {
           jio_snprintf(buf, buflen, "%s", pSymbol->Name);
