4b9f06ee1a2087f1399986b1042050c67f135b53
Dmitriy Kuminov <coding@dmik.org>
2012-06-06 13:58:02 +0000

    icedtea-web: Make sure Jars build for install target.

--- icedtea-web/netx/net/sourceforge/jnlp/cache/CacheUtil.java
+++ icedtea-web/netx/net/sourceforge/jnlp/cache/CacheUtil.java
@@ -363,6 +363,7 @@ public class CacheUtil {
             lruHandler.lock();
             lruHandler.load();
 
+System.out.println("--- 1" + cacheDir);
             File cacheFile = null;
             for (long i = 0; i < Long.MAX_VALUE; i++) {
                 String path = cacheDir + File.separator + i;
