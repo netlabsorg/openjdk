fdc326c8802a9a4224ffc7f828fa7679fe780456
dmik <dmik@93fcbb24-f35c-df11-b929-00e081727c95>
2013-01-28 23:43:05 +0000

    jdk: Fix broken file open dialog in AWT apps.
    It couldn't be dismissed and would not let navigate through dirs.

--- trunk/openjdk/jdk/src/windows/native/sun/windows/awt_Window.h
+++ trunk/openjdk/jdk/src/windows/native/sun/windows/awt_Window.h
@@ -34,6 +34,7 @@
 // property name tagging windows disabled by modality
 static LPCTSTR ModalBlockerProp = TEXT("SunAwtModalBlockerProp");
 static LPCTSTR ModalDialogPeerProp = TEXT("SunAwtModalDialogPeerProp");
+static LPCTSTR ModalDialogProcProp = TEXT("SunAwtModalDialogProcProp");
 
 #ifndef WH_MOUSE_LL
 #define WH_MOUSE_LL 14
